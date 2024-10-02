30e5b89a22f75a0b2ee4978546b5d25bc0380cb5
Meelis Luks a.k.a Mella <mella@lux.ee>
2024-10-02 22:48:39 +0300

    service worker edit

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,20 @@
 self.addEventListener('install', (event) => {
     console.log('Service Worker installing.');
-    // Eelhäälesta vajalikud ressursid
+    event.waitUntil(
+        caches.open('bingo-cache').then(function(cache) {
+            return cache.addAll([
+                'index.html',
+                'css/style.css',
+                'css/bootstrap/bootstrap.min.css',
+                'js/jquery/jquery-3.7.1.min.js',
+                'js/bootstrap/bootstrap.bundle.js',
+                'js/main.js',
+                'data.json',
+                'img/icon-192x192.png',
+                'img/icon-512x512.png'
+            ]);
+        })
+    );
 });
   
 self.addEventListener('activate', (event) => {
